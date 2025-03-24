91297bebd236acf1c2bc6d933c970709ecbba6b0
noktturnal <vojta.matt@gmail.com>
2025-03-24 12:43:16 +0100

    server.js and package.json files updated

--- app/server/server.js
+++ app/server/server.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const { Client } = require('pg');
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to the database
+const client = new Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  
+  client.connect();
+
+// Function to get weather data from OpenWeatherMap API
+async function getWeatherData(city) {
+    const apiKey = process.env.WEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await axios.get(url);
+    return response.data;
+}
